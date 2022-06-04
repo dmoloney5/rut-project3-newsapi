e6dbb7e0c28ffd349fd4fd42ecbb5fda6069dc59
Salieu Sarr <95061054+Ssrr17@users.noreply.github.com>
2022-06-03 21:12:30 -0400

    stripe integration

--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
- const stripe = require("stripe")('sk_test_51L5eeWEeZMz10B1GjVl4Wh0UzFbshuIYEfqnOTauyyGK0eXRLigx05CNATKWmGMRjfIyA7rvssttVevEdKhUepkP00Vvl0Jpze');
 const routes = require('./routes');
-//const uuid = require('uuid/v4')
-// const cors = require('cors')
+const Payment = require('./routes/payment')
+
 
 
 
@@ -13,7 +12,7 @@ const PORT = process.env.PORT || 3001;
 //middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use (cors())
+
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -21,6 +20,9 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(routes);
+app.use(Payment)
+
+
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
