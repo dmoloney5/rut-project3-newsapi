10de3043c15a9f66620437a028c28ed392350681
Angelo <angelocsaverino@gmail.com>
2022-06-04 00:53:28 -0400

    theme test

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,21 @@
-import './App.css';
-import News from "./components/News";
+// import './App.css';
+// import News from "./components/News";
 
-const App = () => <News />;
+import { Flex, Heading, VStack } from "@chakra-ui/react";
+
+// const App = () => <News />;
+
+
+function App(){
+    return(
+    <VStack>
+        <Flex w="100%">
+         <Heading
+         ml="8" size="md" fontWeight='semibold' color="cyan.400">test</Heading>   
+        </Flex>
+    </VStack>
+
+    );
+}
 
 export default App;
