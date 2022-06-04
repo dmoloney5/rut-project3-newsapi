9b4c35dd8b027defceb04eaad3be1eb134212a8f
Salieu Sarr <95061054+Ssrr17@users.noreply.github.com>
2022-06-04 18:57:22 -0400

    stripe integration sample donation works

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,54 @@
-import React, {useState} from 'react';
-import './App.css'
-import StripeCheckout from 'react-stripe-checkout'
-
-
+import React, { useState } from "react";
+//   import logo from "./logo.svg";
+import "./App.css";
+import StripeCheckout from "react-stripe-checkout";
+// import Button from "react-bootstrap/Button";
 
 function App() {
+  const [item] = useState({
+    name: "Donation",
+    price: "10",
+  });
 
-    const[item,setItem] = useState({
-        
+  const payment = (token) => {
+    const body = {
+      token,
+      item,
+    };
+    const headers = {
+      "Content-Type": "application/json",
+    };
+    return fetch(`http://localhost:8282/payment`, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(body),
     })
-    return(
-        <header>
-        <div>
-            <p>
-                filler for now
-            </p>
-            
-        </div>
-        <StripeCheckout/>
-        </header>
-    )
+      .then((response) => {
+        console.log("RESPONSE", response);
+        const { status } = response;
+        console.log("STATUS", status);
+      })
+      .catch((error) => console.log(error));
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <StripeCheckout
+          stripeKey={process.env.REACT_APP_KEY}
+          token={payment}
+          name="Donation"
+          amount={item.price * 100}
+        >
+          <button>Donate ${item.price}</button>
+        </StripeCheckout>
+      </header>
+    </div>
+  );
 }
 
 export default App;
