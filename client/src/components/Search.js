e52dbfc2b7835b0664461f3e71d20f3ac36b796f
Hannah Bush <heb.rutgers@gmail.com>
2022-06-05 13:06:59 -0400

    took validation out of search

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,24 +1,11 @@
-import React, { useLayoutEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 function SearchBar() {
   //jsx
   const [barState, setBarState] = useState({ searchTarget: '' });
-  const [errMsg, setErrMsg] = useState('');
+  //const [errMsg, setErrMsg] = useState('');
   function handleChange(e) {
-    if (e.target.searchTarget === '') {
-      const isValid = validateEmail(e.target.value);
-      console.log(isValid);
-      // isValid conditional statement
-      if (!isValid) {
-        setErrMsg('Your search is invalid.');
-      } else {
-        setErrMsg('');
-      }
-      console.log('errorMessage:', errMsg);
-    }
-    if (!errorMessage) {
-      setBarState({ ...barState, [e.target.searchTarget]: e.target.value });
-    }
+    setBarState({ ...barState, [e.target.searchTarget]: e.target.value });
   }
   function handleSubmit(e) {
     e.preventDefault();
