87ab494f9822841677142c602c5de033a1765c5d
Hannah Bush <heb.rutgers@gmail.com>
2022-06-05 13:25:27 -0400

    basic login front-end created

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -14,6 +14,15 @@ const LoginForm = () => {
     const handleFormSubmit = async (event) => {
         event.preventDefault();
     }
-
-    
+    return (
+        <div className = 'Login'>
+            <h1>Login</h1>
+            <form onSubmit={handleFormSubmit}>
+                <label>Email</label>
+                <input type="text" name="email" required />
+                <label>Password</label>
+                <input type="text" name="password" required />
+            </form>
+        </div>
+    )
 }
