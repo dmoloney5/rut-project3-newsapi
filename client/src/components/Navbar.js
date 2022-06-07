1e3c284e79816f2661dcfcd2912cb5ba38d56603
techman <dmoloney5@gmail.com>
2022-06-07 19:50:02 -0400

    pushing what we have to heroku

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,15 +7,16 @@ function Nav({ currentPage, handlePageChange, setSearchResults }) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a class="navbar-brand" href="#/"
+        <a class="navbar-brand" 
           onClick={() => handlePageChange('About')}>Null and Void News</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <SearchBar
+         <SearchBar
           handlePageChange={handlePageChange}
           setSearchResults={setSearchResults}
         />
+       
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
